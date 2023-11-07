733a200addcb6289c6283f10d02693bbe576b74b
Meng Chen <meng.chen03@gmail.com>
2023-11-07 17:06:33 -0500

    added main py file

---
+++ serve.py
@@ -0,0 +1,19 @@
+import os
+from fastapi import Request
+from utils import InputData, OutputData, Server
+
+# initiate App
+server = Server()
+app = server.create_app()
+
+# check health
+@app.get("/health")
+async def check_health():
+    return {"response": "healthy"}
+
+# post
+@app.post("/GPT-3.5/", response_model=OutputData)
+async def get_response(request: Request, input_data: InputData):
+    prompt = input_data.prompt
+    response = server.get_chat_response(prompt)
+    return OutputData(response=response)
